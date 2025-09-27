eb8f4ebc495f7167293032298cf7f4e9dc0e2f5b
Ronak99 <punase.ronak99@gmail.com>
2025-09-27 16:45:58 +0530

    Initial Commit

---
+++ StackBot/StackBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackBotApp.swift
+//  StackBot
+//
+//  Created by Ronak Punase on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct StackBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StackBotTests/StackBotTests.swift
@@ -0,0 +1,17 @@
+//
+//  StackBotTests.swift
+//  StackBotTests
+//
+//  Created by Ronak Punase on 27/09/25.
+//
+
+import Testing
+@testable import StackBot
+
+struct StackBotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
